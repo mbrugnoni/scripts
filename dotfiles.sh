1d442df5450841ade93cec33f9251d89e58d43de
Michael Brugnoni <michael.brugnoni@gmail.com>
2021-12-31 15:19:45 -0500

    Updates dotfiles script

--- dotfiles.sh
+++ dotfiles.sh
@@ -9,10 +9,16 @@
 dotfiles=( ".bashrc" ".profile" ".tmux.conf" )
 
 # Clone dotfiles from git repo
-git clone https://github.com/mbrugnoni/dotfiles.git ~/dotfiles
+echo "Prompting for GitHub ssh key passphrase...\n"
+if [[ ! -d ~/dotfiles ]]
+then
+  git clone https://github.com/mbrugnoni/dotfiles.git ~/dotfiles
+ else
+  git -C ~/dotfiles pull 
+fi
 
 # Backup current dotfiles and copy new ones in from git repo
-for $i in "${dotfiles[@]}"
+for i in "${dotfiles[@]}"
 do
 	cp ~/$i ~/bkp
 	cp ~/dotfiles/$i ~
